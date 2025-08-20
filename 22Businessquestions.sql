92b71bb6d6050cf505c5ac6d066a1087281f1410
Rohit Raj Singh <rohit517929@gmail.com>
2025-08-20 06:34:04 +0530

    Add files via upload

---
+++ 22Businessquestions.sql
@@ -0,0 +1,45 @@
+📌 Questions List (Q1 → Q22)
+
+1.Retrieve all books in the "Science Fiction" genre.
+
+2.Find books published after the year 1980.
+
+3.List all customers from Turkey.
+
+4.Show orders placed in November 2023.
+
+5.Retrieve the total stock of books available.
+
+6.Retrieve the average stock of books available.
+
+7.Retrieve the count of stock entries of books available.
+
+8.Find the details of the most expensive book.
+
+9.Show all customers who ordered more than 1 quantity of a book.
+
+10.Retrieve all orders where the total amount exceeds $40.
+
+11.List all genres available in the Books table.
+
+12.Find the book(s) with the lowest stock.
+
+13.Calculate the total revenue generated from all orders.
+
+14.Retrieve the total number of books sold for each genre.
+
+15.Find the average price of books in the "Fantasy" genre.
+
+16.List customers who have placed at least 2 orders.
+
+17.Find the most frequently ordered book.
+
+18.Show the top 5 most expensive books of the "Fantasy" genre.
+
+19.Retrieve the total quantity of books sold by each author.
+
+20.List the cities where customers who spent over $20 are located.
+
+21.Find the customer who spent the most on orders.
+
+22.Calculate the stock remaining after fulfilling all orders.
